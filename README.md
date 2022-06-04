75ad215ffdcc9b224d63af56474238635f043279
nfoert <nofoert@gmail.com>
2022-06-04 16:11:00 +0100

    Removing the old files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Jade Physical Edition
-I'm going to try to make an Alexa-like Jade. In this repo, the self-updating files are stored.
--- jadesay.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def jadesay(input):
-    print("JadeSay: ")
-    print(input)
-    os.system('gtts-cli --nocheck "' + input + '" | mpg123 -q -')
